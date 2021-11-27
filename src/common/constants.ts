fad3ed02e9221a8063246ccd3b997af77d4fe3cc
Vova Gridnev <gridnevmami@gmail.com>
2021-11-27 20:42:20 +0300

    feat: create App and initial settings

---
+++ src/common/constants.ts
@@ -0,0 +1,23 @@
+export const technology = [
+    {
+        HTML: 'HTML'
+    },
+    {
+        CSS: 'CSS'
+    },
+    {
+        JS: 'JS/TS'
+    },
+    {
+        React: 'React'
+    },
+    {
+        Git: 'Git'
+    },
+    {
+        Express: 'Express.js'
+    },
+    {
+        Mongo: 'MongoDB'
+    },
+]
