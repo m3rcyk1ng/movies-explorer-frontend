069180b21847900246e5b38092476188f4188079
Vova Gridnev <gridnevmami@gmail.com>
2021-12-12 16:47:04 +0300

    bugfix: refactor styles after review

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -9,19 +9,19 @@ function Portfolio() {
        <h4 className="portfolio__title">{portfolio.Title}</h4>
        <ul className="portfolio__links">
            <li className="portfolio__link_container">
-               <a className="portfolio__link" href="https://google.com">
+               <a className="portfolio__link" href="https://github.com/m3rcyk1ng/how-to-learn" target="_blank">
                    <p className="portfolio__name">{portfolio.Static}</p>
                    <div className="portfolio__icon" />
                </a>
            </li>
            <li className="portfolio__link_container">
-               <a className="portfolio__link" href="https://google.com">
+               <a className="portfolio__link" href="https://github.com/m3rcyk1ng/russian-travel" target="_blank">
                    <p className="portfolio__name">{portfolio.Adaptive}</p>
                    <div className="portfolio__icon" />
                </a>
            </li>
            <li className="portfolio__link_container">
-               <a className="portfolio__link" href="https://google.com">
+               <a className="portfolio__link" href="https://github.com/m3rcyk1ng/react-mesto-auth" target="_blank">
                    <p className="portfolio__name">{portfolio.SPA}</p>
                    <div className="portfolio__icon" />
                </a>
